ed1aa0e2327ebb242d7aad28bb75ac15d5f93296
Minori Nakasone <s18010@std.it-college.ac.jp>
2018-06-14 04:37:16 +0000

    うるう年(仮)

---
+++ 0614/061401.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xv
+
+year=$1
+mod4=$(($year%4))
+mod100=$(($year%100))
+mod400=$(($year%400))
+[ $mod400 -eq 0 ] || [ $mod100 -ne 0 ] && [ $mod4 -eq 0 ]
+leap_year=$?
+if [ $leap_year -eq 0 ]; then
+    echo "$1年はうるう年です。"
+else:
+    echo "$1年はうるう年ではありません。"
+fi
