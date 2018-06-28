0de551226c033fa6b58368b61edabfb72484b5e9
Minori Nakasone <s18010@std.it-college.ac.jp>
2018-06-28 01:37:57 +0000

    出力結果の順番の修正

--- 0628/062804.sh
+++ 0628/062804.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat personal_infomation.csv | awk -F'[,]' '{print $7}' | sort | uniq -c | sort -nr | head -n 5
+cat personal_infomation.csv | awk -F'[,]' '{print $7}' | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2,$1}'
